d61f71b410696ac7fd272eb9e7f9f95d59fdc415
Robb Marsee <robb.marsee@gamil.com>
2016-06-24 12:08:48 -0700

    "complete React mini-sprint"

--- app.js
+++ app.js
@@ -1,1 +1,52 @@
-// TODO
+class Item extends React.Component {
+	constructor(props) {
+		super(props)
+		
+		this.state = {
+			done: false
+		}
+	}
+
+
+	onListHover() {
+		this.setState({
+			done: !this.state.done
+		})
+	}
+
+	render() {
+		var style = {
+			fontWeight: this.state.done ? 'bold' : 'normal'
+		}
+
+		return (
+			<li style={style} onMouseOver={this.onListHover.bind(this)} >{this.props.items} </li>
+		)
+	}
+}
+
+
+var GroceryList = (props) => {
+
+	return (
+	<ul>
+		{props.items.map(items =>
+			<Item items={items} />
+			)}
+	</ul>
+	)
+};
+
+
+var App = () => (
+	<div>
+		<h2>My Grocery List</h2>
+		<GroceryList items={['fat', 'protein']}/>
+	</div>
+);
+
+var items = ['fat', 'protein']
+
+// var groceryItem = ['fat', 'protein']
+
+ReactDOM.render(<App />, document.getElementById('app'));
